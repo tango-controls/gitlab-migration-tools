3084c8827aa5eb45835f659c1a24127327ecc4e4
Carlos Pascual <cpascual@cells.es>
2021-01-29 09:25:26 +0100

    Use --orphan

--- add_moved_branch.sh
+++ add_moved_branch.sh
@@ -9,9 +9,9 @@ BRANCH="moved-to-gitlab"
 set -x
 git clone $ORIG/$PROJECT
 cd $PROJECT
-git checkout -b $BRANCH
-git rm -r .
+git checkout --orphan $BRANCH
+git rm -rf .
 echo "## $PROJECT moved to $DEST/$PROJECT" > README.md
 git add .
 git commit -m "Move to $DEST/$PROJECT"
-git push origin $BRANCH
+#git push origin $BRANCH
